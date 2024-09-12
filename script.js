9385bc3c78c8312db47c0da687b02029ed19715e
kadyjapb <163013058+kadyjapb@users.noreply.github.com>
2024-09-12 11:51:43 -0300

    corrigindo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.mudarTextoBotao').addEventListener('click ', function(){
+document.querySelector('.mudarTextoBotao').addEventListener('click', function(){
     const paragrafo = document.querySelector('.descricao');
     paragrafo.textContent = 'Texto alterado!';
 });
